ca3f54c9ac3c65735eeb5b5bc65a7061c2d67c92
Nirajan <nirajanshrestha672@gmail.com>
2025-01-28 19:13:48 -0500

    [Section 1.0.3] Github Authentication(Sign in ,sign out, navbar)

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,47 @@
+
+import Link from "next/link";
+import Image from "next/image";
+import * as React from "react";
+import { auth, signIn, signOut } from "@/auth";
+
+const Navbar = async () => {
+    const session = await auth();  
+    return (
+        <div className="px-5 py-3 bg-white shadow-sm font-work-sm">
+            <nav className="flex justify-between items-center">
+                <Link href="/">
+                    <Image src="/logo.png" alt="Logo" width={144} height={30}/>
+                </Link>
+
+                <div className="flex items-center gap-5 text-black">
+                     {session && session?.user ? (
+                        <>
+                          <Link href="/startup/create">
+                          <span>Create</span>
+                          </Link>
+                          <form action={async() => {
+                               'use server'
+                               await signOut
+                          }}><button type="submit">Logout</button></form>
+
+                          <Link href={`/user/${session?.user.id}`}>
+                            <span>{session?.user?.name}</span>
+                          </Link>
+                        </>
+                     ): (<>
+                        <form action={async ()=>
+                            { 
+                                'use server'
+                                await signIn('github')
+                            }}>
+                            <button type="submit">Login</button>
+                        </form>
+                     </>)}
+                </div>
+            </nav>
+
+        </div>
+    )
+}
+
+export default Navbar
